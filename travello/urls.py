a205f5decaa39c7d081710223f6362e86132cf1a
subhamdash1998 <mrsubhamdash1998@gmail.com>
2020-07-24 12:56:23 +0530

    first commit

---
+++ travello/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+urlpatterns=[
+     path('',views.index,name='index'),
+     path('home',views.home,name='home'),
+     path('contact',views.contact,name='contact')
+]
